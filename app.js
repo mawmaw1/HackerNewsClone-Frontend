787dc2c626e0036aca096a7a2b093fd06ab57a63
Magnus Larsen <kontakt@magnuslarsen.dk>
2017-09-19 17:52:12 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+new Vue({
+
+    // We want to target the div with an id of 'events'
+    el: '#events',
+
+    // Here we can register any values or collections that hold data
+    // for the application
+    data: {
+        text: "Brakuk"
+    },
+
+
+    // Methods we want to use in our application are registered here
+    methods: {
+
+        getText: function(){
+
+            this.$http.get('http://localhost:8081/api')
+                .then(function(text){
+                    this.text = text;
+                }, function (error) {
+
+                })
+        }
+
+    },
+        mounted: function () {
+            this.getText();
+        }
+
+}
+
+
+
+);
